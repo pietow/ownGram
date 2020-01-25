ab3ca8328842cbc5226d27d9ede150f5b5e00b68
pietow <pprotassow@posteo.de>
2020-01-25 17:52:57 +0100

    pics included

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" style="border:solid black">
+<div class="container">
     <div class="row">
-        <div class="col-3" style="border:solid red">
 
-            <img src="/svg/atom.svg" class="rounded-circle ml-5"; style="height:150px; border:solid red"> 
-
+        <div class="col-3">
+            <img src="/svg/atom.svg" class="rounded-circle ml-5"; style="height:150px"> 
         </div>
+
         <div class="col-9">
             <div><h1>myInstaGram</h1></div>
-
             <div class="d-flex">
                 <div class="pr-3"><strong>123</strong> posts</div>
                 <div class="pr-3"><strong>23k</strong> followers</div>
                 <div class="pr-3"><strong>231</strong> following</div>
             </div>
-
+            <div class="pt-4 font-weight-bold">www.me.com</div>
+            <div>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+                 Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+                  Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </div>
+            <div><a href="#">www.me.com</a></div>    
         </div>
+
     </div>
+
+    <div class="row pt-5">
+        <div class="col-4"><img src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" class="w-100"></div>
+        <div class="col-4"><img src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" class="w-100"></div>
+        <div class="col-4"><img src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" class="w-100"></div>
+         </div>
 </div>
-@endsection
+@endsection 
