5459b333bc394e62d015edde6da34fc0c6a9902b
pietow <pprotassow@posteo.de>
2020-01-26 21:06:19 +0100

    connect Profile and user tables

--- app/Profile.php
+++ app/Profile.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    //fetch from User model
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
