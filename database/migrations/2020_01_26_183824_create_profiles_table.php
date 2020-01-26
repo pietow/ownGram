f5397ae0fc144e29f8482bfd23e7858dfd174167
pietow <pprotassow@posteo.de>
2020-01-26 21:06:45 +0100

    creating profile table

--- database/migrations/2020_01_26_183824_create_profiles_table.php
+++ database/migrations/2020_01_26_183824_create_profiles_table.php
@@ -15,7 +15,18 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            //connect to the users table via reference to user table
+            $table->unsignedBigInteger('user_id');
+           // $table->string('title')->nullable();
+           // $table->text('description')->nullable();
+           // $table->string('url')->nullable();
             $table->timestamps();
+
+            //for searchability and quicker queries 
+ 
+ 
+            //index for any foreign key
+            $table->index('user_id');
         });
     }
 
