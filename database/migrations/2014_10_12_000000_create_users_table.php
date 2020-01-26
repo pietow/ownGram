974ac5b8f21b20bfbd10ae340c1eaa87a65c64c6
pietow <pprotassow@posteo.de>
2020-01-26 18:11:49 +0100

    insert username into database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();//unique constraint at the database level
+            $table->string('username')->unique();//must be unique
+            
+            $table->timestamp('email_verified_at')->nullable(); //nullable it is optional
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
