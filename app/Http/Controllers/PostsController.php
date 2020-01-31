1e933e05a7e7cd4769e9927d1cb72884d3754a6b
pietow <pprotassow@posteo.de>
2020-01-31 18:55:24 +0100

    post  created and graped for view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -39,9 +39,8 @@ public function __construct()
 
     $data = request()->validate([
       'caption' => 'required',
-      'image' => 'required', 'image',
-    ]);
-    
+      'image'   => ['required', 'image'],
+    ]); 
     //like in tinker:
    // $post = new \App\Post();
    // $post->caption = $data['caption'];
@@ -49,13 +48,23 @@ public function __construct()
 
    //shorter \App\Post::create($data); throws an Integrity constraint violation error, because id is not passed
 
+    $imagePath = request('image')->store('uploads', 'public'); //store(PATH, driver: e.g. s3); s3 for amazon storage
+//laravel creates in storage/app/public a uploads directory with the image file
+
+  // but storage/app/public is not accessable -->  storage:link = Create a symbolic link from "public/storage" to "storage/app/public"
+
+
    //grap the authenticated user then go into their posts and create, laravel graps the user id automatically
-   auth()->user()->posts()->create($data);
+   //auth()->user()->posts()->create($data);
+   auth()->user()->posts()->create([
+     'caption' => $data['caption'],
+     'image'   => $imagePath,
+   ]);
 
-
+  return redirect('/profile/' . auth()->user()->id);  
     
     //retrieve all of the input data as an array using the all method
-    dd(request()->all());
+    //dd(request()->all());
 
    // dd(request()->input("caption"));
    // dd(request()->file('image'));
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,33 +5,36 @@
     <div class="row">
 
         <div class="col-3">
-            <img src="/svg/atom.svg" class="rounded-circle ml-5"; style="height:150px"> 
+            <img src="/svg/atom.svg" class="rounded-circle ml-5" ; style="height:150px">
         </div>
 
 
         <div class="col-9">
             <!--mustache syntax for echoing out-->
             <div class="d-flex justify-content-between align-items-baseline">
-            <h1>{{ $user->username }}</h1>
-            <a href="#">Add New Post</a>
+                <h1>{{ $user->username }}</h1>
+                <a href="/p/create">Add New Post</a>
             </div>
 
             <div class="d-flex">
-                <div class="pr-3"><strong>123</strong> posts</div>
+                <div class="pr-3"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-3"><strong>23k</strong> followers</div>
                 <div class="pr-3"><strong>231</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{$user-> profile->title}}</div>
             <div>{{$user->profile->description ?? 'arrr'}}</div>
-            <div><a href="#">{{$user->profile->url ?? 'N/A'}}</a></div>    
+            <div><a href="#">{{$user->profile->url ?? 'N/A'}}</a></div>
         </div>
 
     </div>
 
     <div class="row pt-5">
-        <div class="col-4"><img src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" class="w-100"></div>
-        <div class="col-4"><img src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" class="w-100"></div>
-        <div class="col-4"><img src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" class="w-100"></div>
-         </div>
+        @foreach ($user->posts as $post)
+        <div class="col-4 pb-4"><img
+                src="http://127.0.0.1:8000/storage/{{$post->image}}";
+                class="w-100">
+            </div>
+    @endforeach
+    </div>
 </div>
-@endsection 
+@endsection
