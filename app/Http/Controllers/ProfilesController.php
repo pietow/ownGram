b527f9b1c6460bb8254865855d406d9cddd28aae
pietow <pprotassow@posteo.de>
2020-02-03 17:45:48 +0100

    edit and update

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -8,16 +8,27 @@ use App\User;
 class ProfilesController extends Controller
 {
     //gives id of the user
-    public function index($user)
+    public function index(User $user)
     {
         //dd = die and dump
         //dd(User::find($user));
-        $user = User::findOrFail($user);
+        //$user = User::findOrFail($user);
 
         //pass data to the view
         //directory view ---> profiles ---> index.blade.php
-        return view('profiles.index', [
-            'user' => $user,
-        ]);
+        //return view('profiles.index', ['user' => $user,]);
+        return view('profiles.index', compact('user'));
     }
+
+    public function edit(User $user)
+    {
+        return view('profiles.edit', compact('user'));
+    }
+    
+ // public function test(User $user)
+ // {
+ //     //return view('profiles.bla', ['user'=> $user]);
+ //     return view('profiles.test', compact('user'));
+ // }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,19 @@ Route::get('/', function () {
 Auth::routes();
 
 
+//{post}- variabel is going to hit our PostsController 
+Route::get('/p/{post}', 'PostsController@show');
+
 //goes to the PostsController and hits the create method
 Route::get('/p/create', 'PostsController@create');
 
 Route::post('/p', 'PostsController@store');
 
+
 //restful Resource Controller with Action show
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+//that will show us the form
+Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
+//that will do the process of updating
+Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
+//Route::get('/profile/{user}', 'ProfilesController@test');
