f25ab77425933075941c7a9e7e4a8010bb0eb271
pietow <pprotassow@posteo.de>
2020-02-04 18:15:21 +0100

    edit event via policies

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Intervention\Image\Facades\Image;
+
 
 class ProfilesController extends Controller
 {
@@ -22,11 +24,14 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
+        //this edit view is now protected
+        $this->authorize('update', $user->profile);
         return view('profiles.edit', compact('user'));
     }
 
    public function update(User $user)
    {
+       $this->authorize('update', $user->profile);
        $data = request()->validate([
            'title' => 'required',
            'description' => 'required',
@@ -34,10 +39,23 @@ class ProfilesController extends Controller
            'image' => '',
        ]);
 
-   auth()->user()->profile->update($data);
+
+   if (request('image')){
+
+    $imagePath = request('image')->store('profile', 'public');
+
+    $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);  //fit(width, height)
+    $image->save();
+
+    $imageArray =   ['image' => $imagePath ?? ' '];
+   }
+   //array_merge: merges two arrays and replaces former key value pairs (e.g. 'image' => '' becomes 'image'=>$imagePath)
+   auth()->user()->profile->update(array_merge(
+       $data,
+       $imageArray ?? []        
+    ));
 
    return redirect('profile/' . $user->id);
-       
    } 
  // public function test(User $user)
  // {
